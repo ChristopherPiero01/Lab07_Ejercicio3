3fbd64ec09081e2b961ab0445c78618f30a472f3
Christopher Bellido <pierobellido1901@gmail.com>
2023-10-04 15:15:09 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp57/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp57
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int añoinicial = 1900;
+            int añofinal = 2023;
+
+            Console.WriteLine("Años bisiestos desde 1900...");
+            Console.WriteLine();
+
+            do
+            {
+                if (añoinicial % 4 == 0 && añoinicial % 100 != 0 || añoinicial % 400 == 0)
+                {
+                    Console.Write(añoinicial+ ", ");
+                }
+                añoinicial++;
+
+            } while (añoinicial <= añofinal);
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
